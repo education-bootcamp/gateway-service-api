3adb030080bfb1714ce03ba06e154c1f3e5351b9
ozen-hub <info.ozenhub@gmail.com>
2024-03-13 20:46:34 +0530

    2

--- src/main/java/com/devstack/lms/gatewayserviceapi/GatewayServiceApiApplication.java
+++ src/main/java/com/devstack/lms/gatewayserviceapi/GatewayServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.devstack.lms.gatewayserviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApiApplication {
 
 	public static void main(String[] args) {
